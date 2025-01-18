01b46afbdda95ca21354a58ef9dc6a86b2329c85
Juan Pimentel <juandbpimentel@gmail.com>
2025-01-18 19:03:13 -0300

    base do projeto finalizada

---
+++ trabalho-pratico-2/main.py
@@ -0,0 +1,18 @@
+#TODO: FUNCIONARIO
+#       - TODO: MODELO
+#       - TODO: CONTROLLER
+#       - TODO: CRUD_ROUTER
+#TODO: DEPARTAMENTO
+#TODO: PROJETO
+#TODO: CLIENTE
+#TODO: CONTRATO
+
+from fastapi import FastAPI
+from database import Base,engine
+from models.funcionario_model import funcionario_routes
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(funcionario_routes)
